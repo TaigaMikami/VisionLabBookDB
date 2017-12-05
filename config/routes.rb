c93c8e89d3c39574ab77e45ce8809f15dc4e0f94
TaigaMikami <mikamimikami60@gmail.com>
2017-12-06 04:04:13 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     root 'static_pages#home'
     get '/about', to: 'static_pages#about'
-    resources :books #do
-      # collection do
-      #   get 'search'
-      # end
-    # end
+    resources :books do
+      collection do
+        get 'search'
+      end
+    end
 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
