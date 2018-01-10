8115c86ebd7f710b2bcd44847433e24bbf00cad1
TaigaMikami <mikamimikami60@gmail.com>
2018-01-11 01:23:34 +0900

    add validation image_nam and add error_messages

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :place, presence: true
+  validates :image_name, format: /\A#{URI::regexp(%w(http https))}\z/
 
   default_scope -> { order(title: :desc) }
 
